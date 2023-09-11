357a0a29dbe73f6a5979bbfba32750312a6646a3
KhathutsheloCarter <121690232+KhathutsheloCarter@users.noreply.github.com>
2023-09-11 15:03:23 +0200

    Add files via upload

---
+++ User input/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+
+            Console.Write("Enter your Age");
+            int age = convert.ToInt32(Console.ReadLine());
+
+            Console.Write("you have " + age + "years");
+
+           
+            Console.ReadLine();
+        }
+    }
+}
